da89e6114cb9f06c98898fac69d11a7b45498991
Andy <andrewcunnell@hotmail.co.uk>
2019-10-07 17:24:17 +0100

    Add an Environment Controller and get configuration from environment variables

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,27 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    private Map<String, String> envVars = new HashMap<>();
+
+    public EnvController(@Value("${port:NOT SET}") String port,
+                         @Value("${memory.limit:NOT SET}") String memoryLimit,
+                         @Value("${cf.instance.index:NOT SET}") String cfInstanceIndex,
+                         @Value("${cf.instance.addr:NOT SET}") String cfInstanceAddress) {
+        envVars.put("PORT", port);
+        envVars.put("MEMORY_LIMIT", memoryLimit);
+        envVars.put("CF_INSTANCE_INDEX", cfInstanceIndex);
+        envVars.put("CF_INSTANCE_ADDR", cfInstanceAddress);
+    }
+
+    public Map<String, String> getEnv() {
+        return envVars;
+    }
+}
--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,12 +1,20 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+
+    private String _welcomeMessage;
+
+    public WelcomeController(@Value("${welcome.message}") String message){
+        _welcomeMessage = message;
+    }
+
     @GetMapping("/")
-    public String GetHello(){
-        return "hello";
+    public String sayHello() {
+        return _welcomeMessage;
     }
 }
